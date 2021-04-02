c006bf36ecd227b9f208617f2257289b2009a948
Nimsi123 <nimsi@berkeley.edu>
2021-04-02 14:05:26 -0700

    Restore default data_files in master branch. Diverge from branch 'nima'.

--- data_files/api_keys.py
+++ data_files/api_keys.py
@@ -1,9 +1,4 @@
 api_keys = [
-	'bfb3cb210e50c39d09f82432095a5150', #nimarahmanian2020@gmail.com
-	'cbbdd094d7401d8912b09341e37be9b1', #nimarahmanian8@gmail.com
-	'c733663048589db82005534b6739c32e', #nimsi@berkeley.edu
-	'10c2e4d0fef8e45470a5b43b84f15ec0', #oxaxe7@gmail.com
-	'81d0339948cd0596cf05a03df5b32288', #rahmanian.arya2356@gmail.com
-	'042d872c6185752c4b3db850014bace1', #nikolas-cacerces
-	"7b3ed1376b2358ebf50609d891ace0b4", #nimarahmanianstorage1@gmail.com
+    "<add api key>",
+    "<optionally add more keys to the list>"
 ]
